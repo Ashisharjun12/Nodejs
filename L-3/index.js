5af6865a646c3552ab970a555ef6128b4e03b2bf
Ashish Raj <ashishrahul748@gmail.com>
2024-05-25 02:32:54 +0530

    making server & handeling urls

--- L-3/index.js
+++ L-3/index.js
@@ -1,34 +1,32 @@
-const http = require("http")
-const fs = require('fs')
+const http = require("http");
+const fs = require("fs");
+const url = require('url')
 
-const myserver = http.createServer((req,res)=>{
+const myserver = http.createServer((req, res) => {
+  const log = `${Date.now()}  ${req.url}: new req recieved\n`;
+  console.log("new req receive");
+  const myurl = url.parse(req.url ,true)
+  console.log(myurl)
+  fs.appendFile("log.txt", log, (err, data) => {
+    if (err) console.log(err);
+    else {
+      switch (myurl.pathname) {
+        case "/":
+          res.end("hello homepage");
+          break;
 
-   const log = `${Date.now()}  ${req.url}: new req recieved\n`
-    console.log('new req receive');
-    fs.appendFile('log.txt' , log ,(err ,data)=>{
+        case "/about":
+            const user = myurl.query.myname
+          res.end(`hello , ${user}`);
+          break;
 
-        if(err ) console.log(err)
-            else {
+        default:
+          res.end("404 not found");
+      }
+    }
+  });
+});
 
-                switch (req.url) {
-                    case '/':
-                        res.end('hello homepage')
-                        break;
-                
-                    case '/about':
-                        res.end('helloo from about')
-                        break;
-        
-                        default :
-                        res.end('404 not found')
-        }
-       
-        }
-        res.end('hello from server')
-    })
-   
-})
-
-myserver.listen(9000,()=>{
-    console.log('server is running at : 9000')
-})
+myserver.listen(9000, () => {
+  console.log("server is running at : 9000");
+});
